835d5057e67542fb177236303cef8de23fc72fc1
Alvaro Graves <alvaro@graves.cl>
2013-05-13 23:03:47 -0700

    Integrating ideas from badgercl

--- console.js
+++ console.js
@@ -30,6 +30,7 @@ Console = {
 	divId: "#consolediv",
 	parentId: "body",
 	loglevel: 1, //for future use
+	events: {},
 	init: function(divId, parentId, config){
 		var self = this;
 		if(parentId != undefined){
@@ -43,6 +44,24 @@ Console = {
 			$(self.parentId).append("<div id='"+id+"' class='_consolebox'></div>");
 			$(self.parentId).append("<div id='"+id+"button' class='_consolebutton'>Hide log window</div>");
 		}
+		if(config.events != undefined){
+			$.each(config.events, function(action, matchStrings){
+				$.each(matchStrings, function(i, matchString){
+					var matchedElements = $(matchString);
+					$.each(matchedElements, function(j, item){
+						$item = $(item);
+						$item.on(action, function(e){
+							var id = "";
+							if(e.target.id != null && e.target.id != ""){
+								id = " with id '"+e.target.id+"'";
+								console.log($(e.target));
+							}
+							Console.log("Event '"+action+"' triggered by "+e.target.nodeName+id); 
+					    });
+					})
+				});				
+			});
+		}
 		//Event for hiding page
 		$(self.divId+"button").on('click', function(){
 			if($(self.divId).is(":visible")){
@@ -54,20 +73,13 @@ Console = {
 			}
 		});
 		setLogs(config);
-	},
-	
+	},	
 	log: function(msg, s){
 		var self = this;
 		var source = (s != undefined)?s+": " : "";
-	    $(self.divId).append("<p> "+source+msg+"</p>");   
+		$(self.divId).append("<p> "+source+msg+"</p>");   
 		var n = $(self.divId).get(0).scrollHeight;
 		$(self.divId).animate({ scrollTop: n }); 
-	},
-	
-	logElement: function(element, message){
-		var id = "&lt;Id not set&gt;";
-		if(element.id !== "") id = element.id;
-		Console.log(message, id + '('+element.tagName+')');
 	}	
 }
 
